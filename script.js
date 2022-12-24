6d75cfd619800ed2cd40fbef2e2ec301bfe3c723
KausVlad <kausvirtual@gmail.com>
2022-12-24 13:39:19 +0200

    No number!

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// console.log(document.querySelector('.between').textContent);
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'test 1234 🎂';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 22;
+
+// console.log((document.querySelector('.guess').value = 7));
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
